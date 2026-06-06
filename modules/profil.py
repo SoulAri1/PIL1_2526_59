b1417a6969b431523d0090e84f06d69a9ec8e6df
elielmensah021 <elielmensah021@gmail.com>
2026-06-06 19:29:53 +0100

    Update profil.py

--- modules/profil.py
+++ modules/profil.py
@@ -1,3 +1,40 @@
-from flask import Blueprint
-profil_bp = Blueprint('profil', __name__)
-# Routes de profil à compléter
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# Simulation de base de données
+profils = []
+
+@app.route('/')
+def accueil():
+    return render_template('profil.html')
+
+
+@app.route('/valider_profil', methods=['POST'])
+def valider_profil():
+
+    nom = request.form.get('nom')
+    prenom = request.form.get('prenom')
+    filiere = request.form.get('filiere')
+
+    competences = request.form.getlist('competences')
+    lacunes = request.form.getlist('lacunes')
+
+    profil = {
+        "nom": nom,
+        "prenom": prenom,
+        "filiere": filiere,
+        "competences": competences,
+        "lacunes": lacunes
+    }
+
+    profils.append(profil)
+
+    return render_template(
+        'confirmation.html',
+        profil=profil
+    )
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
